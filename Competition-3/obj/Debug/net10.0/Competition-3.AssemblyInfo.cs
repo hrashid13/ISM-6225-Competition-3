036842fc3a6a755d65153474b6420bafbe1f0853
gustavows <gustavow@usf.edu>
2026-04-22 11:13:58 -0400

    Add Article model for News API

--- Competition-3/obj/Debug/net10.0/Competition-3.AssemblyInfo.cs
+++ Competition-3/obj/Debug/net10.0/Competition-3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Competition-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dedf7a365d9bbf7b44fc84c01bef0ad3f846b17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1933ad9690d74bdbbdf5bc4c531b71475318c96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Competition-3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Competition-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
