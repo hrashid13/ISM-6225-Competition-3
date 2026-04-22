4dedf7a365d9bbf7b44fc84c01bef0ad3f846b17
Hesham Rashid <h.f.rashid@gmail.com>
2026-04-22 09:05:06 -0400

    Added MVC Framework

---
+++ Competition-3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Competition_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
